9f4d4ffe702e893f4f645b33c0f2c74cc423c227
witktom5 <witktom5@gmail.com>
2022-11-17 21:26:17 +0100

    setup project

---
+++ apps/ui/next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/',
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
